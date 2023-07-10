cea9fea43ddfcf1b0e153d375052d0cfdfbc0588
Akshar Parmar <akshar.parmar@somaiya.edu>
2023-07-10 18:37:03 +0530

    Added rabbitmq config

--- src/index.js
+++ src/index.js
@@ -5,12 +5,18 @@ const {PORT} = require('./config/serverConfig');
 const {sendBasicEmail} = require('./services/email-service');
 const jobs = require('./utils/jobs');
 const TicketController = require('./controller/ticket-controller');
+const {createChannel} = require('./utils/messageQueue');
 
 
-const setUpServer = ()=>{
+const setUpServer = async()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+    const channel = await createChannel();
+
+
+    
+    
     app.post('/api/v1/tickets',TicketController.create);
     
     
